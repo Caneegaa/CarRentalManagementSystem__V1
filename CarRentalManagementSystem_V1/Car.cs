68900b6aa918d65b8feaa928446524d767e86dfa
ut01426 <ut01426@UNICOMTIC24>
2024-10-11 14:42:36 +0530

    Question 3

--- CarRentalManagementSystem_V1/Car.cs
+++ CarRentalManagementSystem_V1/Car.cs
@@ -8,5 +8,25 @@ namespace CarRentalManagementSystem_V1
 {
     internal class Car
     {
+        private string CarId { get; set; }
+        private string Brand { get; set; }
+        private string Model { get; set; }
+        private decimal RentalPrice { get; set; }
+
+        public Car(string carId, string brand, string model, decimal rentalPrice)
+        {
+            CarId = carId;
+            Brand = brand;
+            Model = model;
+            RentalPrice = rentalPrice;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {CarId}, Brand: {Brand}, Model: {Model}, RentalPrice: {RentalPrice}";
+        }
+
+        
+
     }
 }
--- CarRentalManagementSystem_V1/Program.cs
+++ CarRentalManagementSystem_V1/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace CarRentalManagementSystem_V1
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace CarRentalManagementSystem_V1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var car1 = new Car("CAR_001", "TOYOTA", "prius", 10);
+            Console.WriteLine(car1);
+            Console.ReadLine();
         }
     }
 }
